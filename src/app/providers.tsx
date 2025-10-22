06623f5307cb93c14786504ba94564db11f49709
Sam Tagalog <98756288+samtags@users.noreply.github.com>
2025-10-23 05:05:11 +0800

    feat: change api key

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,7 +3,7 @@
 import Brook from "@aptly-sdk/brook";
 import { BrookProvider } from "@aptly-sdk/brook/react";
 
-const API_KEY = "pk_qgtoa1gGuj2hei6vyMPvHwOfxkf0tY7JrKykAMxE";
+const API_KEY = "pk_zKHqrVh8N23aqK1Q8BIDHIv7uy3bBqubS7u19Qc3";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   const config = new Brook({
